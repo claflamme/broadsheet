a80f389fb49dfb91f2e370f5494b0a29694500a9
Cory <me@corylaflamme.com>
2016-03-18 15:00:23 -0400

    Removed previous, commented-out Feed model.

--- api/models/Feed.js
+++ api/models/Feed.js
@@ -40,27 +40,3 @@ Schema.statics.findOutdated = function(cb) {
 };
 
 module.exports = Schema;
-
-// # module.exports =
-// #
-// #   subscribers: (subscriberId) ->
-// #
-// #     subscribers = @belongsToMany 'User', 'subscriptions'
-// #
-// #     if subscriberId
-// #       subscribers = subscribers.query 'where', 'id', '=', subscriberId
-// #
-// #     return subscribers
-// #
-// #   articles: ->
-// #
-// #     @hasMany 'Article'
-// #
-// #   outdated: ->
-// #
-// #     @query (queryBuilder) ->
-// #       queryBuilder
-// #       .whereRaw 'updated_at = created_at'
-// #       .orWhere 'updated_at', null
-// #       .orWhere 'updated_at', '<', getThreshold()
-// #       .orderBy 'updated_at', 'asc'
